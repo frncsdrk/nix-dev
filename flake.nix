90ac8f1264e959bd307f64d01868362d265be2b4
frncsdrk <git@frncsdrk.me>
2023-11-20 20:08:46 +0100

    [feat] Add ziglang dev flake template

--- flake.nix
+++ flake.nix
@@ -8,6 +8,14 @@
           path = ./golang;
           description = "golang";
         };
+        nodejs = {
+          path = ./nodejs;
+          description = "nodejs";
+        };
+        ziglang = {
+          path = ./ziglang;
+          description = "ziglang";
+        };
       };
     };
 }
