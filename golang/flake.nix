a78da4cfb7922033461b675b1e4a96b091975871
frncsdrk <git@frncsdrk.me>
2023-11-20 19:44:28 +0100

    [fix] Fix golang flake

--- golang/flake.nix
+++ golang/flake.nix
@@ -13,7 +13,7 @@
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       eachSystem = nixpkgs.lib.genAttrs supportedSystems;
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = eachSystem (system: import nixpkgs { inherit system; });
     in {
       # Dev dependencies
       devShells = eachSystem (system:
