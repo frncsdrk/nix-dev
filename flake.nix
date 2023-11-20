0ea6273ec7553e87ed667809e5e6395b6d81d1b3
frncsdrk <git@frncsdrk.me>
2023-11-20 18:47:03 +0100

    [chore] Simplify main flake
    Can it really be so simple?

--- flake.nix
+++ flake.nix
@@ -1,30 +1,32 @@
 {
   description = "Nix flake dev templates";
 
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
-  };
+  # inputs = {
+  #   nixpkgs = {
+  #     url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  #   };
+  # };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      eachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in {
-      devShells = eachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ ];
-        };
-      });
-    }
-    //
+  outputs = { self }:
+    # let
+    #   supportedSystems = [ "x86_64-linux" ];
+    #   eachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+    #     pkgs = import nixpkgs { inherit system; };
+    #   });
+    # in {
+    #   devShells = eachSupportedSystem ({ pkgs }: {
+    #     default = pkgs.mkShell {
+    #       packages = with pkgs; [ ];
+    #     };
+    #   });
+    # }
+    # //
     {
-      templates = rec {
-        path = ./golang;
-        description = "golang";
+      templates = {
+        golang = {
+          path = ./golang;
+          description = "golang";
+        };
       };
     };
 }
