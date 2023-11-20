0c6cd8d4d73af5e476ce276053039afda22e89da
frncsdrk <git@frncsdrk.me>
2023-11-20 18:49:57 +0100

    [chore] Clean up
    It really can be that simple.

--- flake.nix
+++ flake.nix
@@ -1,26 +1,7 @@
 {
   description = "Nix flake dev templates";
 
-  # inputs = {
-  #   nixpkgs = {
-  #     url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  #   };
-  # };
-
   outputs = { self }:
-    # let
-    #   supportedSystems = [ "x86_64-linux" ];
-    #   eachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-    #     pkgs = import nixpkgs { inherit system; };
-    #   });
-    # in {
-    #   devShells = eachSupportedSystem ({ pkgs }: {
-    #     default = pkgs.mkShell {
-    #       packages = with pkgs; [ ];
-    #     };
-    #   });
-    # }
-    # //
     {
       templates = {
         golang = {
