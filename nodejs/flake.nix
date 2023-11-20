b7653a9655a16caba296ef11ab233ef84916730d
frncsdrk <git@frncsdrk.me>
2023-11-20 19:44:45 +0100

    [feat] Add nodejs dev flake

---
+++ nodejs/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      eachSystem = nixpkgs.lib.genAttrs supportedSystems;
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = eachSystem (system: import nixpkgs { inherit system; });
+    in {
+      # Dev dependencies
+      devShells = eachSystem (system:
+        let 
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_20
+              nodePackages.pnpm
+            ];
+          };
+        });
+    };
+}
